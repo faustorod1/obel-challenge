bbff1b3307f8f3d8134867835a189099072bf3d6
Faus <faustoandresrodriguez@gmail.com>
2026-02-08 16:27:03 -0300

    añado validacion de token undefined en middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 export const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization;
-  if (token === process.env.AUTH_TOKEN) {
+  if (token && token === process.env.AUTH_TOKEN) {
     next();
   } else {
     res.status(401).json({ message: "No autorizado: Token faltante o inválido" });
