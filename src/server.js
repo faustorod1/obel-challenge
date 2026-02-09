9d9e6a89abc07225c26814c5afcd9f5a7509381a
Faus <faustoandresrodriguez@gmail.com>
2026-02-09 02:33:02 -0300

    correccion de log en terminal

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
+const APP_URL = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
+
 app.listen(PORT, () => {
-  console.log(`Obel Challenge API corriendo en http://localhost:${PORT}`);
-  console.log(`Documentación en http://localhost:${PORT}/docs`);
+  console.log(`Obel Challenge API corriendo en ${APP_URL}`);
+  console.log(`Documentación en ${APP_URL}/docs`);
 });
